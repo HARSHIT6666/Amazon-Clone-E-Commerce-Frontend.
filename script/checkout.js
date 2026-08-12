923ca2159d4b0ac00f10044ddc3c238d14a2a180
HARSHIT <harshitrautela411@gmail.com>
2026-08-12 19:16:25 +0530

    delete product from html when clicking delete

--- script/checkout.js
+++ script/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem)=>{
     cartSummaryHTML+=
     // console.log(matchingProduct);
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -103,5 +103,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   removefromcart(productId);
   // console.log(cart);
 
+
+ const container =  document.querySelector(`.js-cart-item-container-${productId}`);
+ container.remove();
   })
 })
