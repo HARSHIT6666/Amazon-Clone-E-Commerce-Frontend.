cbbf232310927aea8cf8c2f142bd03a0b882d5ff
HARSHIT <harshitrautela411@gmail.com>
2026-08-20 10:12:10 +0530

    some addtocart test using jasmine framework

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,16 @@
+import {addtocart , cart ,loadFromStorage} from '../../data/cart.js';
+import { product } from '../../data/products.js';
+describe ('test suite : addtocart' , ()=>{
+    it('adds an existing product to the cart' , ()=>{
+     spyOn(localStorage,'getItem').and.callFake(()=>{
+        return JSON.stringify([{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity : 1,
+            deliveryOption : '1'
+        }]);
+     });
+      loadFromStorage();
+
+    });
+   
+});
--- tests-jasmine/utils/moneytest.js
+++ tests-jasmine/utils/moneytest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../script/utils/money.js";
+import { formatCurrency } from "../../script/utils/money.js";
 describe('test suite  : formatCurrenecy' , ()=>{
     it('converse cents into dollar' ,()=>{
         expect(formatCurrency(2095)).toEqual('20.95');
