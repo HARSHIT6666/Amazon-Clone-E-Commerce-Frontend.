450810b8fc2d0307e83eef312951ca6c8a182794
HARSHIT <harshitrautela411@gmail.com>
2026-08-22 10:35:15 +0530

    create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,96 @@
+ function Cart(localStoragekey){
+ const cart =  {
+    cartItems  : undefined,
+    loadFromStorage(){
+   this.cartItems = JSON.parse(localStorage.getItem(localStoragekey));
+ if(!this.cart){
+
+ this.cart = [{
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity : 2,
+    deliveryOptionId :'1'
+ },
+ {
+productId :  "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+quantity: 1,
+ deliveryOptionId :'2'
+ }
+];
+ }},
+
+
+  saveToStorage(){
+  localStorage.setItem(localStoragekey,JSON.stringify(this.cart));
+},
+
+ addtocart(productId){
+ let matchingItem;
+
+ this.cart.forEach((cartItem)=>{
+  if(productId === cartItem.productId){
+    matchingItem=cartItem;
+  }
+ });
+
+ const QuantitySelector =document.querySelector(`.js-quantity-selector-${productId}`);
+ const finalSelectedQuantity = Number(QuantitySelector);
+
+ if(matchingItem){
+  matchingItem.quantity +=finalSelectedQuantity;
+ }else{
+  this.cart.push({
+  productId  : productId,
+  quantity : finalSelectedQuantity,
+  deliveryOptionId : '1'
+
+ });
+
+ }
+ this.saveToStorage();
+}, 
+
+ removefromcart(productId){
+  const newCart = [];
+  this.cart.forEach((cartItem)=>{
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  })
+  this.cart =newCart;
+  this.saveToStorage();
+}, 
+
+ updateQuantity(productId , newQuantity){
+  let matchingItem;
+  this.cart.forEach((cartItem)=>{
+  if(productId === cartItem.productId){
+    matchingItem=cartItem;
+  }
+ });
+ matchingItem.quantity = newQuantity;
+ this.saveToStorage();
+}
+ };
+ return cart;
+ }
+ const cart = Cart('cart-oop');
+ const businessCart = Cart('cart-business');
+
+ cart.loadFromStorage();
+businessCart.loadFromStorage();
+
+ console.log(cart);
+ console.log(businessCart);
+
+ 
+
+
+
+
+
+
+
+
+
+
+ 
--- script/checkout.js
+++ script/checkout.js
@@ -1,7 +1,8 @@
 import { renderCheckOutHeader } from "./checkout/checkOutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/backend-practice.js'
+import '../data/cart-oop.js';
+import'../data/backend-practice.js';
  
 renderCheckOutHeader();
 renderOrderSummary();
