cedf629277e5eb22cd0a58de3975dc5463a3b3e7
HARSHIT <harshitrautela411@gmail.com>
2026-08-22 08:41:01 +0530

    add backend practice file

--- script/checkout.js
+++ script/checkout.js
@@ -1,6 +1,7 @@
 import { renderCheckOutHeader } from "./checkout/checkOutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/backend-practice.js'
  
 renderCheckOutHeader();
 renderOrderSummary();
