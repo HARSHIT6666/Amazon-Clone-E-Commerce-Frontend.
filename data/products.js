a51ba41d4866c9c2ba283710d2805afefcaf359c
HARSHIT <harshitrautela411@gmail.com>
2026-08-22 18:33:16 +0530

    date in class/object

--- data/products.js
+++ data/products.js
@@ -54,6 +54,10 @@ extrainfoHTML(){
 
 }
 
+ /*const date = new Date();
+ console.log(date);
+console.log( date.toLocaleTimeString());  */
+
  export const product = [
 {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
